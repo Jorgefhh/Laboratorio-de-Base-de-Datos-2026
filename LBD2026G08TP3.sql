13eee784f9f5d915040e1e986c5591edb376361a
russoagustin <pickrusso.agustin@gmail.com>
2026-06-08 16:15:15 -0300

    Corrección de errores

--- LBD2026G08TP3.sql
+++ LBD2026G08TP3.sql
@@ -12,7 +12,8 @@ CREATE PROCEDURE crear_usuario(
     IN p_dni CHAR(8),
     IN p_email VARCHAR(255),
     IN p_username VARCHAR(45),
-    IN p_contrasenia CHAR(60)  -- Nota: Idealmente ya debe venir encriptada (ej. BCrypt)
+    IN p_contrasenia CHAR(60),  -- Nota: Idealmente ya debe venir encriptada (ej. BCrypt)
+    OUT mensaje VARCHAR(255)
 )
 BEGIN
     DECLARE datos_invalidos CONDITION FOR SQLSTATE '45000';
@@ -20,48 +21,71 @@ BEGIN
     
     -- VALIDACIONES 
     IF (p_nombres IS NULL) OR (p_apellidos IS NULL) OR (p_fechaNac IS NULL) OR (p_dni IS NULL) OR (p_email IS NULL) OR (p_username IS NULL) OR (p_contrasenia IS NULL) THEN
-        SIGNAL datos_invalidos SET MESSAGE_TEXT = 'Error, debe llenar los campos obligatorios', MYSQL_ERRNO = 45000;
+        SET mensaje = 'Error, debe llenar los campos obligatorios';
+        SIGNAL datos_invalidos SET MESSAGE_TEXT = mensaje, MYSQL_ERRNO = 45000;
+        
     ELSEIF (TRIM(p_nombres) = '') THEN
-        SIGNAL datos_invalidos SET MESSAGE_TEXT = 'Error, el nombre no puede ser vacío', MYSQL_ERRNO = 45000;
+        SET mensaje = 'Error, el nombre no puede ser vacío';
+        SIGNAL datos_invalidos SET MESSAGE_TEXT = mensaje, MYSQL_ERRNO = 45000;
+        
     ELSEIF (TRIM(p_apellidos) = '') THEN
-        SIGNAL datos_invalidos SET MESSAGE_TEXT = 'Error, el apellido no puede ser vacío', MYSQL_ERRNO = 45000;
-	ELSEIF (p_fechaNac > CURDATE()) THEN
-		SIGNAL datos_invalidos SET MESSAGE_TEXT = 'Error, la fecha de nacimiento no puede ser una fecha futura', MYSQL_ERRNO = 45000;
+        SET mensaje = 'Error, el apellido no puede ser vacío';
+        SIGNAL datos_invalidos SET MESSAGE_TEXT = mensaje, MYSQL_ERRNO = 45000;
+        
+    ELSEIF (p_fechaNac > CURDATE()) THEN
+        SET mensaje = 'Error, la fecha de nacimiento no puede ser una fecha futura';
+        SIGNAL datos_invalidos SET MESSAGE_TEXT = mensaje, MYSQL_ERRNO = 45000;
+        
     ELSEIF (TRIM(p_email) = '') THEN
-        SIGNAL datos_invalidos SET MESSAGE_TEXT = 'Error, el email no puede ser vacío', MYSQL_ERRNO = 45000;
+        SET mensaje = 'Error, el email no puede ser vacío';
+        SIGNAL datos_invalidos SET MESSAGE_TEXT = mensaje, MYSQL_ERRNO = 45000;
+        
     ELSEIF (TRIM(p_username) = '') THEN
-        SIGNAL datos_invalidos SET MESSAGE_TEXT = 'Error, el user no puede ser vacío', MYSQL_ERRNO = 45000;
+        SET mensaje = 'Error, el user no puede ser vacío';
+        SIGNAL datos_invalidos SET MESSAGE_TEXT = mensaje, MYSQL_ERRNO = 45000;
+        
     ELSEIF (TRIM(p_contrasenia) = '') THEN
-        SIGNAL datos_invalidos SET MESSAGE_TEXT = 'Error, la contraseña no puede ser vacía', MYSQL_ERRNO = 45000;
-	ELSEIF EXISTS(SELECT 1 FROM Usuarios WHERE dni = p_dni) THEN
-			SIGNAL datos_invalidos SET MESSAGE_TEXT = 'Error, el DNI ya está en  uso', MYSQL_ERRNO = 45000;
+        SET mensaje = 'Error, la contraseña no puede ser vacía';
+        SIGNAL datos_invalidos SET MESSAGE_TEXT = mensaje, MYSQL_ERRNO = 45000;
+        
+    ELSEIF EXISTS(SELECT 1 FROM Usuarios WHERE dni = p_dni) THEN
+        SET mensaje = 'Error, el DNI ya está en uso';
+        SIGNAL datos_invalidos SET MESSAGE_TEXT = mensaje, MYSQL_ERRNO = 45000;
+        
     ELSEIF EXISTS(SELECT 1 FROM Usuarios WHERE username = p_username) THEN
-        SIGNAL datos_invalidos SET MESSAGE_TEXT = 'Error, el username ya está en uso', MYSQL_ERRNO = 45000;
+        SET mensaje = 'Error, el username ya está en uso';
+        SIGNAL datos_invalidos SET MESSAGE_TEXT = mensaje, MYSQL_ERRNO = 45000;
+        
     ELSEIF EXISTS(SELECT 1 FROM Usuarios WHERE email = p_email) THEN
-        SIGNAL datos_invalidos SET MESSAGE_TEXT = 'Error, el email ya está registrado', MYSQL_ERRNO = 45000;
+        SET mensaje = 'Error, el email ya está registrado';
+        SIGNAL datos_invalidos SET MESSAGE_TEXT = mensaje, MYSQL_ERRNO = 45000;
     END IF;
     
     BLOQUE_TRANSACTION: BEGIN
-    DECLARE EXIT HANDLER FOR SQLEXCEPTION
-    BEGIN
-		ROLLBACK;
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Error interno en el servidor', MYSQL_ERRNO = 45000;
-    END;
-    START TRANSACTION;
-        INSERT INTO Usuarios (nombres, apellidos, fechaNac, dni, email, username, contrasenia, esMozo, esAdmin, activo)
-        VALUES (p_nombres, p_apellidos, p_fechaNac, p_dni, p_email, p_username, p_contrasenia, 0, 0, 0);
+        DECLARE EXIT HANDLER FOR SQLEXCEPTION
+        BEGIN
+            ROLLBACK;
+            SET mensaje = 'Error interno en el servidor al intentar crear el usuario';
+            SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = mensaje, MYSQL_ERRNO = 45000;
+        END;
         
-        SET v_idUsuario = LAST_INSERT_ID(); 
-        
-        -- Inserto en cliente (por defecto todos los usuarios tienen rol cliente)
-        INSERT INTO Clientes(idCliente, puntos) 
-        VALUES (v_idUsuario, 0);
-    COMMIT;
+        START TRANSACTION;
+            INSERT INTO Usuarios (nombres, apellidos, fechaNac, dni, email, username, contrasenia, esMozo, esAdmin, activo)
+            VALUES (p_nombres, p_apellidos, p_fechaNac, p_dni, p_email, p_username, p_contrasenia, 0, 0, 0);
+            
+            SET v_idUsuario = LAST_INSERT_ID(); 
+            
+            -- Inserto en cliente (por defecto todos los usuarios tienen rol cliente)
+            INSERT INTO Clientes(idCliente, puntos) 
+            VALUES (v_idUsuario, 0);
+        COMMIT;
     END BLOQUE_TRANSACTION;
 
+    -- MENSAJE DE ÉXITO (Solo se llega aquí si no saltó ningún SIGNAL)
+    SET mensaje = 'Usuario y cliente creados exitosamente';
     SELECT v_idUsuario AS idUsuarioCreado;
+    
 END//
-
 -- *LLAMADAS DE PRUEBA * --
 
 /* 5.--------------- MODIFICACIÓN DE UN USUARIO ---------------*/
@@ -69,62 +93,90 @@ END//
 DROP PROCEDURE IF EXISTS modificar_usuario//
 
 CREATE PROCEDURE modificar_usuario(
-	IN p_idUsuario INT,
+    IN p_idUsuario INT,
     IN p_nombres VARCHAR(45),
     IN p_apellidos VARCHAR(45),
     IN p_fechaNac DATE,
     IN p_dni CHAR(8),
     IN p_username VARCHAR(45),
-    IN p_contrasenia CHAR(60)  -- Ya debe venir encriptada
+    IN p_contrasenia CHAR(60),  -- Ya debe venir encriptada
+    OUT mensaje VARCHAR(255)    -- Nuevo parámetro de salida
 )
 BEGIN
     DECLARE datos_invalidos CONDITION FOR SQLSTATE '45000';
-    DECLARE v_idUsuario INT;
-	DECLARE v_dni CHAR(8);
+    DECLARE v_dni CHAR(8);
     DECLARE v_username VARCHAR(45);
     
-    SELECT dni into v_dni FROM Usuarios WHERE idUsuario = p_idUsuario;
+    -- Obtenemos los datos actuales para compararlos
+    SELECT dni INTO v_dni FROM Usuarios WHERE idUsuario = p_idUsuario;
     SELECT username INTO v_username FROM Usuarios WHERE idUsuario = p_idUsuario;
-    
-
     
     -- VALIDACIONES 
     IF NOT EXISTS (SELECT 1 FROM Usuarios WHERE idUsuario = p_idUsuario) THEN
-		SIGNAL datos_invalidos SET MESSAGE_TEXT = 'Usuario no encontrado', MYSQL_ERRNO = 45000;
+        SET mensaje = 'Usuario no encontrado';
+        SIGNAL datos_invalidos SET MESSAGE_TEXT = mensaje, MYSQL_ERRNO = 45000;
+        
     ELSEIF (p_nombres IS NULL) OR (p_apellidos IS NULL) OR (p_fechaNac IS NULL) OR (p_dni IS NULL) OR (p_username IS NULL) OR (p_contrasenia IS NULL) THEN
-        SIGNAL datos_invalidos SET MESSAGE_TEXT = 'Error, debe llenar los campos obligatorios', MYSQL_ERRNO = 45000;
+        SET mensaje = 'Error, debe llenar los campos obligatorios';
+        SIGNAL datos_invalidos SET MESSAGE_TEXT = mensaje, MYSQL_ERRNO = 45000;
+        
     ELSEIF (TRIM(p_nombres) = '') THEN
-        SIGNAL datos_invalidos SET MESSAGE_TEXT = 'Error, el nombre no puede ser vacío', MYSQL_ERRNO = 45000;
+        SET mensaje = 'Error, el nombre no puede ser vacío';
+        SIGNAL datos_invalidos SET MESSAGE_TEXT = mensaje, MYSQL_ERRNO = 45000;
+        
     ELSEIF (TRIM(p_apellidos) = '') THEN
-        SIGNAL datos_invalidos SET MESSAGE_TEXT = 'Error, el apellido no puede ser vacío', MYSQL_ERRNO = 45000;
-	ELSEIF (p_fechaNac > CURDATE()) THEN
-		SIGNAL datos_invalidos SET MESSAGE_TEXT = 'Error, la fecha de nacimiento no puede ser una fecha futura', MYSQL_ERRNO = 45000;
+        SET mensaje = 'Error, el apellido no puede ser vacío';
+        SIGNAL datos_invalidos SET MESSAGE_TEXT = mensaje, MYSQL_ERRNO = 45000;
+        
+    ELSEIF (p_fechaNac > CURDATE()) THEN
+        SET mensaje = 'Error, la fecha de nacimiento no puede ser una fecha futura';
+        SIGNAL datos_invalidos SET MESSAGE_TEXT = mensaje, MYSQL_ERRNO = 45000;
+        
     ELSEIF (TRIM(p_username) = '') THEN
-        SIGNAL datos_invalidos SET MESSAGE_TEXT = 'Error, el user no puede ser vacío', MYSQL_ERRNO = 45000;
+        SET mensaje = 'Error, el user no puede ser vacío';
+        SIGNAL datos_invalidos SET MESSAGE_TEXT = mensaje, MYSQL_ERRNO = 45000;
+        
     ELSEIF (TRIM(p_contrasenia) = '') THEN
-        SIGNAL datos_invalidos SET MESSAGE_TEXT = 'Error, la contraseña no puede ser vacía', MYSQL_ERRNO = 45000;
-	ELSEIF (p_dni <> v_dni) THEN
-		IF EXISTS(SELECT 1 FROM Usuarios WHERE dni = p_dni) THEN
-			SIGNAL datos_invalidos SET MESSAGE_TEXT = 'Error, el DNI ya está en  uso', MYSQL_ERRNO = 45000;
-		END IF;
-	ELSEIF (v_username <> p_username) THEN
-		IF EXISTS(SELECT 1 FROM Usuarios WHERE username = p_username) THEN
-			SIGNAL datos_invalidos SET MESSAGE_TEXT = 'Error, el username ya está en uso', MYSQL_ERRNO = 45000;
-		END IF;
+        SET mensaje = 'Error, la contraseña no puede ser vacía';
+        SIGNAL datos_invalidos SET MESSAGE_TEXT = mensaje, MYSQL_ERRNO = 45000;
+        
+    ELSEIF (p_dni <> v_dni) THEN
+        IF EXISTS(SELECT 1 FROM Usuarios WHERE dni = p_dni) THEN
+            SET mensaje = 'Error, el DNI ya está en uso';
+            SIGNAL datos_invalidos SET MESSAGE_TEXT = mensaje, MYSQL_ERRNO = 45000;
+        END IF;
+        
+    ELSEIF (v_username <> p_username) THEN
+        IF EXISTS(SELECT 1 FROM Usuarios WHERE username = p_username) THEN
+            SET mensaje = 'Error, el username ya está en uso';
+            SIGNAL datos_invalidos SET MESSAGE_TEXT = mensaje, MYSQL_ERRNO = 45000;
+        END IF;
     END IF;
 
-    
     BLOQUE_TRANSACTION: BEGIN
-    DECLARE EXIT HANDLER FOR SQLEXCEPTION
-    BEGIN
-		ROLLBACK;
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Error interno en el servidor', MYSQL_ERRNO = 45000;
-    END;
-    
-	UPDATE Usuarios SET nombres = p_nombres, apellidos = p_apellidos, fechaNac = p_fechaNac, dni = p_dni, username = p_username, contrasenia = p_contrasenia
-	WHERE idUsuario = p_idUsuario;
+        DECLARE EXIT HANDLER FOR SQLEXCEPTION
+        BEGIN
+            ROLLBACK;
+            SET mensaje = 'Error interno en el servidor al intentar modificar el usuario';
+            SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = mensaje, MYSQL_ERRNO = 45000;
+        END;
         
+        -- Añadido START TRANSACTION para que el ROLLBACK funcione
+        START TRANSACTION;
+            UPDATE Usuarios 
+            SET nombres = p_nombres, 
+                apellidos = p_apellidos, 
+                fechaNac = p_fechaNac, 
+                dni = p_dni, 
+                username = p_username, 
+                contrasenia = p_contrasenia
+            WHERE idUsuario = p_idUsuario;
+        -- Añadido COMMIT
+        COMMIT;
     END BLOQUE_TRANSACTION;
+
+    -- MENSAJE DE ÉXITO (Solo se llega aquí si no saltó ningún SIGNAL)
+    SET mensaje = 'Usuario modificado exitosamente';
 
 END//
 
